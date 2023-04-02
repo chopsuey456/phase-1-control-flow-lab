e9045c382f22af89c300596ad322d89661924e05
Sara Withrow <saralouise1107@gmail.com>
2023-04-02 15:03:16 -0400

    uploaded

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  } else if (someValue > 2500) {
+      result = 'No can do.';
+  } else if (someValue > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (someValue > 400){ 
+    result = 'That will be twenty bucks.';
+  } ;
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(anotherValue){
+if (anotherValue === 'NYC')
+return 'Ok, sounds good.';
+
+else (anotherValue !== 'NYC')
+return 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(differentValue){
+  const tip = differentValue;
+  let reply;
+
+  switch (tip){
+    case 'generous':
+    reply = "Thank you so much.";
+    break;
+    case 'not as generous':
+    reply = "Thank you."
+    break;
+    default:
+      reply = "Bye."
+
+  }
+
+  return reply;
+
 }
